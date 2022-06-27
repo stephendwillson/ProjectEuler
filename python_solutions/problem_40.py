72bd45b23a4128700d2bb6828265a89b77e977a9
Stephen Willson <stephendwillson@gmail.com>
2022-06-27 00:09:43 -0400

    Problem 40.

---
+++ python_solutions/problem_40.py
@@ -0,0 +1,26 @@
+"""
+https://projecteuler.net/problem=40
+
+An irrational decimal fraction is created by concatenating the positive integers:
+
+0.123456789101112131415161718192021...
+
+sdwillso NOTE: 12th digit post-decimal is highlighted in red.
+It can be seen that the 12th digit of the fractional part is 1.
+
+If d(n) represents the nth digit of the fractional part, find the value of the following expression.
+
+d(1) × d(10) × d(100) × d(1000) × d(10000) × d(100000) × d(1000000)
+"""
+
+ceiling = 1000000
+
+d = ""
+i = 0
+while len(d) < ceiling:
+    i += 1
+    d = d + str(i)
+
+total = int(d[0]) * int(d[9]) * int(d[99]) * int(d[999]) * int(d[9999]) * int(d[99999]) * int(d[999999])
+
+print(total)
