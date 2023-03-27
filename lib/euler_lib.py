c2157a2028014df9502553bf901fa97dcc01659d
Stephen Willson <stephendwillson@gmail.com>
2023-03-27 17:13:21 -0400

    Cleaning code to make linter happy

--- lib/euler_lib.py
+++ lib/euler_lib.py
@@ -2,63 +2,110 @@
 sdwillso
 Common functionality for solving Project Euler problems.
 """
-
+# pylint: disable=invalid-name
 import math
 import string
 
-# collapse triangle upwards, updating bottom row with current maximums
+
+# get_triangle_max_path_sum
 def get_triangle_max_path_sum(t):
-    
+    """
+    Find the sum of the vertical path through a number triangle with the
+    highest max value.
+
+    :param t: Number triangle
+    :type t: [][]
+    :rtype: int
+    """
+
+    # collapse triangle upwards, updating bottom row with current maximums
     for i in range(len(t) - 1, 0, -1):
-        
+
         # make new row of maximums of bottom 2 rows
-        tmp = list()
+        tmp = []
         for j in range(0, len(t[i-1])):
             tmp.append(t[i-1][j] + max(t[i][j], t[i][j+1]))
-        
-        # get rid of current bottom row, replace new bottom with updated maximums
-        t.pop() 
+
+        # get rid of current bottom row
+        # replace new bottom with updated maximums
+        t.pop()
         t[-1] = tmp
-    
+
     return t[0][0]
 
-# add up total for word where A=1, B=2, ..., Y=25, Z=26
+
 def get_word_score(s):
+    """
+    Add up total for word where A=1, B=2, ..., Y=25, Z=26.
+
+    :type s: string
+    :rtype: int
+    """
 
     s = s.upper()
     score = 0
-    
+
     for char in s:
         char_index = string.ascii_uppercase.find(char)
         if char_index != -1:
-            score += char_index + 1 # A == [0] --> A == 1 score
-            
+            score += char_index + 1  # A == [0] --> A == 1 score
+
     return score
 
-# circular prime == all rotations of N are prime, ie 197: 197, 719, 971
+
 def is_circular_prime(n):
+    """
+    Determine if specified number is a circular prime. Circular prime == all
+    rotations of N are prime.
+    Ex. 197: 197, 719, 971.
+
+    :type n: int
+    :rtype: bool
+    """
 
     tmp = str(n)
 
     for i in range(0, len(tmp)):
-        
+
         tmp = rotate(str(n), i)
         if not is_prime(int(tmp)):
             return False
 
     return True
 
-# circular rotate string by N positions
+
 def rotate(s, n):
+    """
+    Rotate string by n positions.
+
+    :type s: string
+    :param n: Number of positions to rotate
+    :type n: int
+    :return: Rotated string
+    :rtype: string
+    """
 
     return s[n:] + s[:n]
 
+
 def is_palindrome(s):
+    """
+    Check if string is a palindrome.
+
+    :type s: string
+    :rtype: bool
+    """
 
     return str(s) == str(s)[::-1]
 
-# leap year checker
+
 def is_leap_year(y):
+    """
+    Check if year is a leap year.
+
+    :type y: int
+    :rtype: bool
+    """
 
     if y % 400 == 0:
         return True
@@ -71,29 +118,47 @@ def is_leap_year(y):
 
     return False
 
-# get number of paths from (0, 0) -> (m, n) only moving right or down
-def count_lattice_paths(n, m):
 
+def count_lattice_paths(n, m):
+    """
+    Get number of paths from (0, 0) -> (m, n) only moving right or down.
+
+    :type n: int
+    :type m: int
+    :rtype: int
+    """
     # n x m lattice paths will always have n + m steps
-    # out of n + m, count all possible downward steps, anything left is rightward
+    # out of n + m, count all possible downward steps
+    # anything left is rightward
     return math.factorial(n + m) // math.factorial(n) // math.factorial(m)
 
-# print n x m grid
-def print_grid(grid): 
- 
+
+def print_grid(grid):
+    """
+    Print n x m grid.
+
+    :type grid: [][]
+    """
+
     if len(grid) == 0:
         print("Grid is empty!")
 
     p = ""
-    for i in range(0, len(grid)):
+    for i in enumerate(grid):
         for ele in grid[i]:
-            p += "{} ".format(ele)
+            p += f"{ele} "
 
         print(p)
         p = ""
 
-# add up all the digits in n
+
 def sum_digits(n):
+    """
+    Add up all the digits in a number.
+
+    :type n: int
+    :rtype: int
+    """
 
     total = 0
 
@@ -102,8 +167,14 @@ def sum_digits(n):
 
     return total
 
-# 1 + 2 + ... + n-1 + n
+
 def get_triangle_number(n):
+    """
+    Add integers from 1 through n.
+
+    :type n: int
+    :rtype: int
+    """
 
     triangle = 0
     for i in range(1, n + 1):
@@ -113,6 +184,13 @@ def get_triangle_number(n):
 
 
 def sum_of_squares(n):
+    """
+    Calculate sum of all squares from 1 through n.
+    1^2 + 2^2 + ... + (n-1)^2 + n^2
+
+    :type n: int
+    :rtype: int
+    """
 
     total = 0
     for i in range(1, n + 1):
@@ -120,7 +198,15 @@ def sum_of_squares(n):
 
     return total
 
+
 def square_of_sums(n):
+    """
+    Calculate the square of sums from 1 through n.
+    (1 + 2 + ... + n-1 + n) ^ 2
+
+    :type n: int
+    :rtype: int
+    """
 
     total = 0
     for i in range(1, n + 1):
@@ -129,9 +215,17 @@ def square_of_sums(n):
     return total * total
 
 # get all factors, not just prime etc.
-def get_factors(n):
 
-    factors = list()
+
+def get_factors(n):
+    """
+    Find all factors of a number.
+
+    :type n: int
+    :rtype: int array
+    """
+
+    factors = []
 
     if n <= 1:
         return factors
@@ -142,21 +236,33 @@ def get_factors(n):
 
             if n / i != i:
                 factors.append(math.floor(n / i))
-    
+
     return factors
 
-# all factors except for self
+
 def get_proper_factors(n):
+    """
+    Find all factors of n except for self.
+
+    :type n: int
+    :rtype: int array
+    """
 
     factors = get_factors(n)
 
     if n in factors:
         factors.remove(n)
-    
+
     return factors
 
-# simple prime checker
+
 def is_prime(n):
+    """
+    Test if a number is prime.
+
+    :type n: int
+    :rtype: bool
+    """
 
     # handle 1, 2, and all even numbers
     if n <= 1:
@@ -173,10 +279,16 @@ def is_prime(n):
 
     return True
 
-# composite number reduction
-def get_prime_factors(n):
 
-    p_factors = list()
+def get_prime_factors(n):
+    """
+    Find all prime factors of a number.
+
+    :type n: int
+    :rtype: int array
+    """
+
+    p_factors = []
 
     c = 2
     while n > 1:
@@ -189,9 +301,18 @@ def get_prime_factors(n):
 
     return p_factors
 
-# get recurring cycle in the decimal for 'numerator / denominator' if one exists, otherwise return None
+
 def get_recurring_decimal_cycle(numerator, denominator):
-    
+    """
+    Find recurring cycle in the decimal for 'numerator / denominator' if
+    one exists.
+
+    :type numerator: int
+    :type denominator: int
+    :return: Numeric cycle if one exists
+    :rtype: int, None
+    """
+
     repeat = ""
 
     # remainder dictionary --> {remainder : position}
@@ -199,7 +320,7 @@ def get_recurring_decimal_cycle(numerator, denominator):
 
     # if remainder ever hits 0, there's no cycle
     remainder = numerator % denominator
-    while(remainder != 0 and remainder not in rem_dict):
+    while (remainder != 0 and remainder not in rem_dict):
 
         rem_dict[remainder] = len(repeat)
 
@@ -208,17 +329,29 @@ def get_recurring_decimal_cycle(numerator, denominator):
 
         remainder %= denominator
 
-    if(remainder == 0):
+    if remainder == 0:
         return None
-    else:
-        return repeat[rem_dict[remainder]:]
 
-# given a list of coin denominations, find the number of coin combos that add up to target
+    return repeat[rem_dict[remainder]:]
+
+
 def num_coin_combos(coins, target):
+    """
+    Find the total number of combinations of coins that add up to the target.
+
+    :param coins: Possible coin values
+    :type coins: int array
+    :param target: Amount coins should add up to
+    :type target: int
+    :rtype: int
+    """
 
     if target < 0 or len(coins) <= 0:
         return 0
     if target == 0:
         return 1
-    
-    return num_coin_combos(coins[:-1], target) + num_coin_combos(coins, target - coins[-1])
+
+    return (
+        num_coin_combos(coins[:-1], target)
+        + num_coin_combos(coins, target - coins[-1])
+    )
--- project_euler.py
+++ project_euler.py
@@ -9,25 +9,36 @@ import sys
 import importlib
 import pathlib
 
-def main():
 
-    problem_help = "Space-separated list of problems. Chooses ALL problems if none are specified."
-    verbose_help = "Prints description of specified problems."
-    solve_help = "Solve the specified problems. Does not solve by default."
-    timer_help = "Display time taken to calculate solution in seconds."
-    validate_help = "Raises ValueError if wrong solution is found."
+def main():
+    """Entry point."""
+
+    problem_help_text = \
+        "Space-separated list of problems. " \
+        "Chooses ALL problems if none are specified."
+    solve_help_text = \
+        "Solve the specified problems. " \
+        "Does not solve by default."
+    verbose_help_text = "Prints description of specified problems."
+    timer_help_text = "Display time taken to calculate solution in seconds."
+    validate_help_text = "Raises ValueError if wrong solution is found."
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--problem", help=problem_help, nargs="+")
-    parser.add_argument("-v", "--verbose", help=verbose_help, action="store_true")
-    parser.add_argument("-s", "--solve", help=solve_help, action="store_true")
-    parser.add_argument("-t", "--timer", help=timer_help, action="store_true")
-    parser.add_argument("--validate", help=validate_help, action="store_true")
+    parser.add_argument("-p", "--problem",
+                        help=problem_help_text, nargs="+")
+    parser.add_argument("-v", "--verbose",
+                        help=verbose_help_text, action="store_true")
+    parser.add_argument("-s", "--solve",
+                        help=solve_help_text, action="store_true")
+    parser.add_argument("-t", "--timer",
+                        help=timer_help_text, action="store_true")
+    parser.add_argument("--validate",
+                        help=validate_help_text, action="store_true")
 
     args = parser.parse_args()
 
     problems = build_problem_list(args.problem)
-    
+
     slowest = 0
     slowest_name = ""
 
@@ -47,78 +58,113 @@ def main():
             total_time = end_time - start_time
             overall_time += total_time
 
-            tidy_problem_name = pathlib.Path(problem).stem.upper().replace("_", " ")
+            tidy_problem_name = \
+                pathlib.Path(problem).stem.upper().replace("_", " ")
 
             if total_time > slowest:
                 slowest = total_time
                 slowest_name = tidy_problem_name
 
-            print("{}: {}".format(tidy_problem_name, solution))
-            
+            print(f"{tidy_problem_name}: {solution}")
+
             if args.timer:
-                print("Time to solve: {0:.4f}s".format(end_time - start_time))
+                print(f"Time to solve: {end_time - start_time:.4f}s")
                 print()
 
     if args.timer:
         print()
-        print("Number of problems solved: {}".format(problem_count))
-        print("Slowest problem to solve: {}".format(slowest_name))
-        print("Time to solve {0}: {1:.2f}s".format(slowest_name, slowest))
-        print("Time for all solutions: {0:.2f}s".format(overall_time))
+        print(f"Number of problems solved: {problem_count}")
+        print(f"Slowest problem to solve: {slowest_name}")
+        print(f"Time to solve {slowest_name}: {slowest:.2f}s")
+        print(f"Time for all solutions: {overall_time:.2f}s")
+
 
 def solve_problem(problem, validate):
+    """
+    Solve the specified problem. Validate solution if specified.
 
-    p_name = pathlib.Path(problem).stem
-    p = importlib.import_module(p_name)
+    :param problem: Problem number
+    :type problem: int
+    :param validate: Check whether solution is correct
+    :type validate: bool
+    :raises ValueError: ValueError is solution is incorrect
+    :rtype: int
+    """
 
-    solution = p.main()
+    problem_name = pathlib.Path(problem).stem
+    problem_script = importlib.import_module(problem_name)
+
+    solution = problem_script.main()
 
     if validate:
-        if solution != p.pe_solution:
-            raise ValueError("WRONG SOLUTION! Expected {} Received {}".format(p.pe_solution, solution))
+        if solution != problem_script.pe_solution:
+            raise ValueError(
+                "WRONG SOLUTION!\n"
+                f"Expected {problem_script.pe_solution}\n"
+                f"Received {solution}"
+                )
 
     return solution
 
+
 def print_problem_info(problem):
+    """
+    Print the problem summary as seen on the Project Euler problem page.
+
+    :param problem: Problem number
+    :type problem: int
+    """
 
     print("=" * 80)
 
-    p_name = pathlib.Path(problem).stem 
-    p = importlib.import_module(p_name)
+    problem_name = pathlib.Path(problem).stem
+    problem_script = importlib.import_module(problem_name)
 
-    print("{} - {}".format(p_name.upper().replace("_", " "), p.pe_name))
-    p.description()
+    print(
+        f"{problem_name.upper().replace('_', ' ')} - {problem_script.pe_name}")
+    problem_script.description()
 
     print("=" * 80)
     print()
 
 
 def build_problem_list(problem_args):
+    """build_problem_list _summary_
+
+    :param problem_args: _description_
+    :type problem_args: _type_
+    :return: _description_
+    :rtype: int array
+    """
 
     p_list = []
-    script_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "python_solutions")
+    script_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "python_solutions")
 
-    # clunky -- this is so imports in solving and printing info work with this script in separate dir
+    # clunky -- this is so imports in solving and printing info work with
+    # this script living in separate dir
     sys.path.append(script_dir)
 
     # if no -p arg is passed in, run all problems
     if problem_args is None:
-        for root, dirs, files in os.walk(script_dir):
+        for _, _, files in os.walk(script_dir):  # root, dir, files
             for file in files:
                 if file.startswith("problem") and file.endswith(".py"):
-                    path = "{}/{}".format(script_dir, file)
+                    path = f"{script_dir}/{file}"
                     p_list.append(path)
 
     else:
-        for p in problem_args:
-            
-            path = "{}/problem_{}.py".format(script_dir, p)
+        for problem_num in problem_args:
+
+            path = f"{script_dir}/problem_{problem_num}.py"
             if os.path.exists(path):
                 p_list.append(path)
 
     # "magic" - sort file names by problem number
     p_list.sort(key=lambda f: int("".join(filter(str.isdigit, f))))
-    
+
     return p_list
+
 
 main()
