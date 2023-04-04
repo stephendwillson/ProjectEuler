745db594793f354a90932727ca0be2f7102e632b
Stephen Willson <stephendwillson@gmail.com>
2023-04-04 19:36:34 -0400

    Problem 23 runtime optimizations

--- python_solutions/problem_23.py
+++ python_solutions/problem_23.py
@@ -9,26 +9,24 @@ def main():
     abundant = []
     for i in range(12, ceiling):
         factors = euler_lib.get_proper_factors(i)
-
         if sum(factors) > i:
             abundant.append(i)
 
-    # calculate all possible sums up front; brute force recalcs took ~1100s
-    # mark index in array for every possible sum of abundants
+    # calculate all possible sums up front
+    # mark index in list for every possible sum of abundants
     # any unmarked index is number that can't be written as sum of 2 abundants
     sums = [False] * (ceiling + 1)
-    for a in abundant:
-        for b in abundant:
-
-            tmp = a + b
-            if tmp >= len(sums):
+    for i, x in enumerate(abundant):
+        for y in abundant[i:]:
+            tmp = x + y
+            if tmp > ceiling:
                 break
             sums[tmp] = True
 
-    # add up every index marked True
+    # add up every index marked False
     total = 0
-    for i in range(1, ceiling):
-        if sums[i] is False:
+    for i in range(1, ceiling+1):
+        if not sums[i]:
             total += i
 
     return total
