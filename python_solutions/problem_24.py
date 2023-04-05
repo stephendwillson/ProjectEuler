ba2ab23600b1aed816b91e0d04a33248d367c5fe
Stephen Willson <stephendwillson@gmail.com>
2023-04-04 20:58:10 -0400

    Problem 24 runtime optimizations

--- python_solutions/problem_24.py
+++ python_solutions/problem_24.py
@@ -1,15 +1,33 @@
-import itertools
+import math
 
 
 def main():
 
-    n = 1000000
+    ceiling = 1000000
+
     digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    permutations = list(itertools.permutations(digits))
-    permutations.sort()
+    permutation = []
+    length = len(digits)
 
-    nth_permutation = int("".join(str(p) for p in permutations[n - 1]))
+    for _ in range(length):
+
+        # calculate the factorial for the remaining digits
+        factorial = math.factorial(length - 1)
+
+        # calculate the index of the digit to add to the permutation
+        index = (ceiling - 1) // factorial
+
+        # add the digit to the permutation and remove from the list of digits
+        digit = digits[index]
+        permutation.append(digit)
+        digits.remove(digit)
+
+        # adjust the ceiling and length for the next iteration
+        ceiling = ceiling - (index * factorial)
+        length = length - 1
+
+    nth_permutation = int("".join(str(d) for d in permutation))
 
     return nth_permutation
 
